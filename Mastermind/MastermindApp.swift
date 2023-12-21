a0e4a2fd662ad20164231072c46aef4b4a1f54bf
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-12-21 09:25:09 +0200

    Initial Commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Ruslan Hladchenko on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
